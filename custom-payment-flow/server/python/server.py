8800d3870d173caa14f2cfea7c5cb23276fd8b88
CJ Avilla <cjavilla@stripe.com>
2021-01-26 12:44:05 -0800

    Update CI config to use correct integration name

--- custom-payment-flow/server/python/server.py
+++ custom-payment-flow/server/python/server.py
@@ -16,11 +16,9 @@ from dotenv import load_dotenv, find_dotenv
 # Setup Stripe python client library
 load_dotenv(find_dotenv())
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
-stripe.api_version = os.getenv('STRIPE_API_VERSION')
 
 static_dir = str(os.path.abspath(os.path.join(__file__ , "..", os.getenv("STATIC_DIR"))))
-app = Flask(__name__, static_folder=static_dir,
-            static_url_path="", template_folder=static_dir)
+app = Flask(__name__, static_folder=static_dir, static_url_path="", template_folder=static_dir)
 
 
 @app.route('/checkout', methods=['GET'])
