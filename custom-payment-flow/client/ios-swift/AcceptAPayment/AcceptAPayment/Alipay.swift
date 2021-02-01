09080569da7246bb12e71c7a5e3bb3fa68f94305
Thorsten Schaeff <thorsten@stripe.com>
2021-02-01 17:36:19 +0800

    Add Stripe

---
+++ custom-payment-flow/client/ios-swift/AcceptAPayment/AcceptAPayment/Alipay.swift
@@ -0,0 +1,20 @@
+//
+//  Alipay.swift
+//  AcceptAPayment
+//
+//  Created by Thorsten Schaeff on 2/1/21.
+//
+
+import SwiftUI
+
+struct Alipay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Alipay_Previews: PreviewProvider {
+    static var previews: some View {
+        Alipay()
+    }
+}
---
+++ custom-payment-flow/client/ios-swift/AcceptAPayment/AcceptAPayment/Card.swift
@@ -0,0 +1,20 @@
+//
+//  Card.swift
+//  AcceptAPayment
+//
+//  Created by Thorsten Schaeff on 2/1/21.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Card_Previews: PreviewProvider {
+    static var previews: some View {
+        Card()
+    }
+}
