0613e9a8b59a2d65d7b7622d4f88ee62a5338755
CJ Avilla <cjavilla@stripe.com>
2021-02-03 09:09:49 -0800

    Add Alipay and GrabPay

--- custom-payment-flow/client/react-cra/src/App.js
+++ custom-payment-flow/client/react-cra/src/App.js
@@ -6,7 +6,9 @@ import { BrowserRouter as Switch, Route } from 'react-router-dom';
 import List from './List';
 
 // Payment method components.
+import Alipay from './Alipay';
 import Bancontact from './Bancontact';
+import GrabPay from './GrabPay';
 import BecsDebit from './BecsDebit';
 import Card from './Card';
 import Eps from './Eps';
@@ -25,16 +27,18 @@ function App(props) {
         <Route exact path="/">
           <List />
         </Route>
+        <Route path="/alipay"><Alipay /></Route>
         <Route path="/bancontact"><Bancontact /></Route>
         <Route path="/becs-debit"><BecsDebit /></Route>
         <Route path="/card"><Card /></Route>
         <Route path="/eps"><Eps /></Route>
         <Route path="/fpx"><Fpx /></Route>
-        <Route path="/ideal"><Ideal /></Route>
-        <Route path="/sepa-debit"><SepaDebit /></Route>
         <Route path="/giropay"><Giropay /></Route>
-        <Route path="/sofort"><Sofort /></Route>
+        <Route path="/grabpay"><GrabPay /></Route>
+        <Route path="/ideal"><Ideal /></Route>
         <Route path="/oxxo"><Oxxo /></Route>
+        <Route path="/sepa-debit"><SepaDebit /></Route>
+        <Route path="/sofort"><Sofort /></Route>
       </Switch>
     </>
   );
