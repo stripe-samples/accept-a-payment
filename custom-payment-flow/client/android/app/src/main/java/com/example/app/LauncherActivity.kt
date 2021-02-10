32dad799990f89357923d02ba8c8acd02e6760be
Ali Riaz <aliriaz@stripe.com>
2021-02-10 16:19:40 -0600

    Added a RecyclerView to LauncherActivity, WIP work on CardActivity and AlipayActivity

--- custom-payment-flow/client/android/app/src/main/java/com/example/app/LauncherActivity.kt
+++ custom-payment-flow/client/android/app/src/main/java/com/example/app/LauncherActivity.kt
@@ -3,22 +3,73 @@ package com.example.app
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_launcher.*
+import androidx.recyclerview.widget.RecyclerView
 
 class LauncherActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle??) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_launcher)
-        launch_checkout_kotlin.setOnClickListener {
-            launchActivity(CheckoutActivityKotlin::class.java)
-        }
-        launch_checkout_java.setOnClickListener {
-            launchActivity(CheckoutActivityJava::class.java)
-        }
+
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = PaymentMethodsListAdapter(this)
     }
 
     private fun launchActivity(activityClass: Class<out Activity>) {
         startActivity(Intent(this, activityClass))
     }
+
+    // Adapter
+    class PaymentMethodsListAdapter(private val activity: Activity)
+        : RecyclerView.Adapter<PaymentMethodsListAdapter.PaymentMethodListViewHolder>() {
+
+        private val items = listOf(
+            PaymentMethodItem(activity.getString(R.string.card_item), CheckoutActivityKotlin::class.java),
+            PaymentMethodItem(activity.getString(R.string.alipay_item), CheckoutActivityKotlin::class.java)
+        )
+
+        private data class PaymentMethodItem constructor(val text: String, val activityClass: Class<*>)
+
+        // Viewholder
+        class PaymentMethodListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val labelView : TextView = itemView.findViewById(R.id.payment_method_type_label)
+
+            fun bind(text: String) {
+                labelView.text = text
+            }
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PaymentMethodListViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.payment_method_item, parent, false)
+
+            return PaymentMethodListViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return items.size
+        }
+
+        override fun onBindViewHolder(holder: PaymentMethodListViewHolder, position: Int) {
+            val paymentMethodItem = items[position]
+            holder.bind(paymentMethodItem.text)
+
+            holder.itemView.setOnClickListener {
+                activity.startActivity(Intent(activity, paymentMethodItem.activityClass))
+            }
+        }
+
+    }
+
 }
