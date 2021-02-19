6f5054efd2ffd030d5ce5a817e502a4038d02152
Thorsten Schaeff <thorsten@stripe.com>
2021-02-19 16:34:22 +0800

    Centralise backendmodel

--- custom-payment-flow/client/ios-swift/AcceptAPayment/AcceptAPayment/Views/Alipay.swift
+++ custom-payment-flow/client/ios-swift/AcceptAPayment/AcceptAPayment/Views/Alipay.swift
@@ -8,57 +8,8 @@
 import SwiftUI
 import Stripe
 
-class MyPIBackendModel : ObservableObject {
-	@Published var paymentStatus: STPPaymentHandlerActionStatus?
-	@Published var paymentIntentParams: STPPaymentIntentParams?
-	@Published var lastPaymentError: NSError?
-
-	func preparePaymentIntent() {
-		// MARK: Fetch the PaymentIntent from the backend
-		// Create a PaymentIntent by calling the sample server's /create-payment-intent endpoint.
-		let url = URL(string: BackendUrl + "create-payment-intent")!
-		let json: [String: Any] = [
-				"currency": "usd",
-				"paymentMethodType": "alipay"
-		]
-		var request = URLRequest(url: url)
-		request.httpMethod = "POST"
-		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.httpBody = try? JSONSerialization.data(withJSONObject: json)
-		let task = URLSession.shared.dataTask(with: request, completionHandler: { [weak self] (data, response, error) in
-				guard let response = response as? HTTPURLResponse,
-						response.statusCode == 200,
-						let data = data,
-						let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any],
-						let clientSecret = json["clientSecret"] as? String else {
-								let message = error?.localizedDescription ?? "Failed to decode response from server."
-								print(message)
-								return
-				}
-				print("Created PaymentIntent")
-				// MARK: Create the PaymentIntent
-				DispatchQueue.main.async {
-					self?.paymentIntentParams = STPPaymentIntentParams(clientSecret: clientSecret)
-				}
-		})
-		task.resume()
-	}
-	
-	func onCompletion(status: STPPaymentHandlerActionStatus, pi: STPPaymentIntent?, error: NSError?) {
-		self.paymentStatus = status
-		self.lastPaymentError = error
-
-		// MARK: Demo cleanup
-		if status == .succeeded {
-			// A PaymentIntent can't be reused after a successful payment. Prepare a new one for the demo.
-			self.paymentIntentParams = nil
-			preparePaymentIntent()
-		}
-	}
-}
-
 struct Alipay: View {
-	@ObservedObject var model = MyPIBackendModel()
+	@ObservedObject var model = BackendModel()
 	@State var isConfirmingPayment = false
 	
 	var body: some View {
@@ -98,7 +49,7 @@ struct Alipay: View {
 					}
 				}
 			}
-		}.onAppear { model.preparePaymentIntent() }
+		}.onAppear { model.preparePaymentIntent(paymentMethodType: "alipay", currency: "usd") }
 	}
 }
 
--- custom-payment-flow/client/ios-swift/AcceptAPayment/AcceptAPayment/Views/Card.swift
+++ custom-payment-flow/client/ios-swift/AcceptAPayment/AcceptAPayment/Views/Card.swift
@@ -6,143 +6,51 @@
 //
 
 import SwiftUI
-import UIKit
 import Stripe
 
 /**
  * This example collects card payments, implementing the guide here: https://stripe.com/docs/payments/accept-a-payment#ios
  */
 
-final class CardViewController: UIViewController {
-    var paymentIntentClientSecret: String?
-
-    lazy var cardTextField: STPPaymentCardTextField = {
-        let cardTextField = STPPaymentCardTextField()
-        return cardTextField
-    }()
-    lazy var payButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.layer.cornerRadius = 5
-        button.backgroundColor = .systemBlue
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 22)
-        button.setTitle("Pay", for: .normal)
-        button.addTarget(self, action: #selector(pay), for: .touchUpInside)
-        return button
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        let stackView = UIStackView(arrangedSubviews: [cardTextField, payButton])
-        stackView.axis = .vertical
-        stackView.spacing = 20
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackView)
-        NSLayoutConstraint.activate([
-            stackView.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: 2),
-            view.rightAnchor.constraint(equalToSystemSpacingAfter: stackView.rightAnchor, multiplier: 2),
-            stackView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 2),
-        ])
-        startCheckout()
-    }
-
-    func displayAlert(title: String, message: String, restartDemo: Bool = false) {
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            if restartDemo {
-                alert.addAction(UIAlertAction(title: "Restart demo", style: .cancel) { _ in
-                    self.cardTextField.clear()
-                    self.startCheckout()
-                })
-            }
-            else {
-                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
-            }
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-
-    func startCheckout() {
-        // Create a PaymentIntent by calling the sample server's /create-payment-intent endpoint.
-        let url = URL(string: BackendUrl + "create-payment-intent")!
-        let json: [String: Any] = [
-            "currency": "usd",
-            "paymentMethodType": "card"
-        ]
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try? JSONSerialization.data(withJSONObject: json)
-        let task = URLSession.shared.dataTask(with: request, completionHandler: { [weak self] (data, response, error) in
-            guard let response = response as? HTTPURLResponse,
-                response.statusCode == 200,
-                let data = data,
-                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any],
-                let clientSecret = json["clientSecret"] as? String else {
-                    let message = error?.localizedDescription ?? "Failed to decode response from server."
-                    self?.displayAlert(title: "Error loading page", message: message)
-                    return
-            }
-            print("Created PaymentIntent")
-            self?.paymentIntentClientSecret = clientSecret
-        })
-        task.resume()
-    }
-
-    @objc
-    func pay() {
-        guard let paymentIntentClientSecret = paymentIntentClientSecret else {
-            return;
-        }
-        // Collect card details
-        let cardParams = cardTextField.cardParams
-        let paymentMethodParams = STPPaymentMethodParams(card: cardParams, billingDetails: nil, metadata: nil)
-        let paymentIntentParams = STPPaymentIntentParams(clientSecret: paymentIntentClientSecret)
-        paymentIntentParams.paymentMethodParams = paymentMethodParams
-
-        // Submit the payment
-        let paymentHandler = STPPaymentHandler.shared()
-        paymentHandler.confirmPayment(paymentIntentParams, with: self) { (status, paymentIntent, error) in
-            switch (status) {
-            case .failed:
-                self.displayAlert(title: "Payment failed", message: error?.localizedDescription ?? "")
-                break
-            case .canceled:
-                self.displayAlert(title: "Payment canceled", message: error?.localizedDescription ?? "")
-                break
-            case .succeeded:
-                self.displayAlert(title: "Payment succeeded", message: paymentIntent?.description ?? "", restartDemo: true)
-                break
-            @unknown default:
-                fatalError()
-                break
-            }
-        }
-    }
-}
-
-extension CardViewController: STPAuthenticationContext {
-    func authenticationPresentingViewController() -> UIViewController {
-        return self
-    }
-}
-
-extension CardViewController: UIViewControllerRepresentable {
-    public typealias UIViewControllerType = CardViewController
-
-    public func makeUIViewController(context: UIViewControllerRepresentableContext<CardViewController>) -> CardViewController {
-        return CardViewController()
-    }
-
-    public func updateUIViewController(_ uiViewController: CardViewController, context: UIViewControllerRepresentableContext<CardViewController>) {
-        //
-    }
-}
-
 struct Card: View {
-    var body: some View {
-        CardViewController()
-    }
+		@ObservedObject var model = BackendModel()
+		@State var isConfirmingPayment = false
+		@State var paymentMethodParams: STPPaymentMethodParams?
+			
+		var body: some View {
+				VStack {
+					STPPaymentCardTextField.Representable(paymentMethodParams: $paymentMethodParams)
+						.padding()
+					if let paymentIntent = model.paymentIntentParams {
+						Button("Buy") {
+							paymentIntent.paymentMethodParams = paymentMethodParams
+							isConfirmingPayment = true
+						}.paymentConfirmationSheet(isConfirmingPayment: $isConfirmingPayment,
+																			 paymentIntentParams: paymentIntent,
+																			 onCompletion: model.onCompletion)
+						.disabled(isConfirmingPayment)
+					} else {
+						Text("Loading...")
+					}
+					if let paymentStatus = model.paymentStatus {
+						HStack {
+							switch paymentStatus {
+							case .succeeded:
+								Image(systemName: "checkmark.circle.fill").foregroundColor(.green)
+								Text("Payment complete!")
+							case .failed:
+								Image(systemName: "xmark.octagon.fill").foregroundColor(.red)
+								Text("Payment failed! \(model.lastPaymentError ?? NSError())")
+							case .canceled:
+								Image(systemName: "xmark.octagon.fill").foregroundColor(.orange)
+								Text("Payment canceled.")
+							@unknown default:
+								Text("Unknown status")
+							}
+						}
+					}
+				}.onAppear { model.preparePaymentIntent(paymentMethodType: "card", currency: "usd") }
+			}
 }
 
 struct Card_Previews: PreviewProvider {
