4f806a0258acc6e13fdcbfd1677e8666508ab28d
CJ Avilla <cjavilla@stripe.com>
2021-03-15 11:23:31 -0700

    Improve handling of missing config.ini file
    fixes #23

--- custom-payment-flow/server/php/public/shared.php
+++ custom-payment-flow/server/php/public/shared.php
@@ -4,17 +4,19 @@ require '../vendor/autoload.php';
 
 // If the config.ini file was not configured properly, display a helpful message.
 if(!file_exists('../config.ini')) {
-  echo "<p>Make a copy of <code>config.ini.sample</code> and name it <code>config.ini</code>, then populate the variables.</p>";
-  echo "<p>It should look something like the following, but contain your <a href='https://dashboard.stripe.com/test/apikeys'>API keys</a>:</p>";
-  echo "<pre>";
-  echo "stripe_secret_key = sk_test_1234...\n";
-  echo "stripe_publishable_key = pk_test_1234...\n";
-  echo "stripe_webhook_secret = whsec_1234...\n";
-  echo "domain = http://localhost:4242\n";
-  echo "</pre>";
-  echo "<hr>";
-  echo "You can use this command to get started:<br>";
-  echo "<pre>cp config.ini.sample config.ini</pre>";
+  ?>
+  <p>Make a copy of <code>config.ini.sample</code> and name it <code>config.ini</code>, then populate the variables.</p>
+  <p>It should look something like the following, but contain your <a href='https://dashboard.stripe.com/test/apikeys'>API keys</a>:</p>
+  <pre>stripe_secret_key = sk_test_1234...
+stripe_publishable_key = pk_test_1234...
+stripe_webhook_secret = whsec_1234...
+domain = http://localhost:4242</pre>
+  <hr>
+
+  <p>You can use this command to get started:</p>
+  <pre>cp config.ini.sample config.ini</pre>
+
+  <?php
   exit;
 }
 
