6740b17cf55c878d046dc97e26cb3d0f827690e6
CJ Avilla <cjavilla@stripe.com>
2021-03-25 12:06:36 -0700

    Use php clients

--- prebuilt-checkout-page/server/php/public/index.php
+++ prebuilt-checkout-page/server/php/public/index.php
@@ -9,7 +9,7 @@ $domain_url = $config['domain'];
 
 // Create new Checkout Session for the order
 // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-$checkout_session = \Stripe\Checkout\Session::create([
+$checkout_session = $stripe->checkout->sessions->create([
   'success_url' => $domain_url . '/success.php?session_id={CHECKOUT_SESSION_ID}',
   'cancel_url' => $domain_url . '/canceled.html',
   'payment_method_types' => [
