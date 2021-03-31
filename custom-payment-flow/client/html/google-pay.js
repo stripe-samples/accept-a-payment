81fa6f427b5df43bd1b1b90028b1b26dbc5d808e
CJ Avilla <cjavilla@stripe.com>
2021-03-30 17:38:51 -0700

    Add google pay to vanillajs

--- custom-payment-flow/client/html/google-pay.js
+++ custom-payment-flow/client/html/google-pay.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       prButton.mount('#payment-request-button');
     } else {
       document.getElementById('payment-request-button').style.display = 'none';
-      addMessage('Apple Pay support not found. Check the pre-requisites above and ensure you are testing in a supported browser.');
+      addMessage('Google Pay support not found. Check the pre-requisites above and ensure you are testing in a supported browser.');
     }
   });
 
@@ -66,6 +66,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     addMessage(`Client secret returned.`);
+
 
     // Confirm the PaymentIntent without handling potential next actions (yet).
     let {error, paymentIntent} = await stripe.confirmCardPayment(
