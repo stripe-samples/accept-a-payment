7a4a793e49a6e4aff1b659ef807dc14aff447c62
CJ Avilla <cjavilla@stripe.com>
2021-04-06 12:20:07 -0700

    Add apple pay instructions to react component

--- custom-payment-flow/client/react-cra/src/ApplePay.js
+++ custom-payment-flow/client/react-cra/src/ApplePay.js
@@ -80,6 +80,17 @@ const ApplePay = () => {
     <>
       <h1>Apple Pay</h1>
 
+      <p>
+        Before you start, you need to:
+        <ul>
+          <li><a href="https://stripe.com/docs/stripe-js/elements/payment-request-button#html-js-testing" target="_blank">Add a payment method to your browser.</a> For example, add a card to your Wallet for Safari.</li>
+          <li>Serve your application over HTTPS. This is a requirement both in development and in production. One way to get up and running is to use a service like <a href="https://ngrok.com/" target="_blank">ngrok</a>.</li>
+          <li><a href="https://stripe.com/docs/stripe-js/elements/payment-request-button#verifying-your-domain-with-apple-pay" target="_blank">Verify your domain with Apple Pay</a>, both in development and production.</li>
+        </ul>
+      </p>
+
+      <a href="https://stripe.com/docs/stripe-js/elements/payment-request-button" target="_blank">Stripe Documentation</a>
+
       {paymentRequest && <PaymentRequestButtonElement options={{paymentRequest}} />}
 
       <StatusMessages messages={messages} />
