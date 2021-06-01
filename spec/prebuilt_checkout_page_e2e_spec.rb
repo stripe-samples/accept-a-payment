c5eef8cc818d9f7bec3c4ceaabc7bdf7eb990d6b
Hibariya <hibariya@gmail.com>
2021-06-02 06:13:58 +0900

    Add e2e tests for prebuilt-checkout-page

---
+++ spec/prebuilt_checkout_page_e2e_spec.rb
@@ -0,0 +1,33 @@
+require 'capybara_support'
+
+RSpec.describe 'Custom payment flow', type: :system do
+  before do
+    visit server_url('/')
+  end
+
+  example 'happy path' do
+    click_on 'Buy'
+
+    fill_in 'email', with: 'test@example.com'
+    fill_in 'cardNumber', with: '4242424242424242'
+    fill_in 'cardExpiry', with: '12 / 33'
+    fill_in 'CVC', with: '123'
+    fill_in 'billingName', with: 'James McGill'
+    select 'United States', from: 'billingCountry'
+    fill_in 'billingPostalCode', with: '10000'
+
+    click_on 'Pay €10.00'
+
+    expect(page).to have_content 'Your payment succeeded'
+    expect(page).to have_content '"amount_total": 1000'
+  end
+
+  example "Cancel a payment" do
+    visit server_url('/')
+
+    click_on 'Buy'
+    click_on 'Previous page'
+
+    expect(page).to have_content 'Your payment was canceled'
+  end
+end
