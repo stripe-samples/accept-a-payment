19011fb6d18bb9f53ea7194cde5e99d8eb669bf0
CJ Avilla <cjavilla@stripe.com>
2021-07-08 14:06:13 -0700

    Bind to 0.0.0.0 for ruby to work with react client

--- custom-payment-flow/server/ruby/server.rb
+++ custom-payment-flow/server/ruby/server.rb
@@ -19,6 +19,7 @@ Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 set :static, true
 set :public_folder, File.join(File.dirname(__FILE__), ENV['STATIC_DIR'])
 set :port, 4242
+set :bind, '0.0.0.0'
 
 get '/' do
   content_type 'text/html'
