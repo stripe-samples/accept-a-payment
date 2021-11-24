cc95d477f17e425872718cdd1c3bfd6e9f9a3968
CJ Avilla <cjavilla@stripe.com>
2021-11-24 14:37:40 -0800

    Adds create react app implementation of payment element

--- payment-element/client/react-cra/src/index.js
+++ payment-element/client/react-cra/src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import reportWebVitals from './reportWebVitals';
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const {publishableKey} = await fetch('/config').then((r) => r.json());
-  const stripePromise = loadStripe(publishableKey, {
-    betas: ['klarna_pm_beta_1'],
-  });
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <Elements stripe={stripePromise}>
-        <App />
-      </Elements>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
