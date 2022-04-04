ea8ee6d5026b02f1e4d8cda343d592412d7bdf7a
Hibariya <hibariya@gmail.com>
2022-04-04 19:47:35 +0900

    E2e tests for payment-element/client/vue-cva
    Added payment-element/client/vue-cva to the e2e test matrix in `ci.yml`.
    Also, made a couple of changes to the Vue app:
    
    * To pass the existing test, changed the "Submit" button to "Pay now" as with other implementations.
    * Checked the package-lock.json in as with other implementations.
    * Applied the following changes to support running on containers:
        * Listen to `0.0.0.0`.
        * Use `SERVER_URL` if it exists.
        * Use `window.location.origin` for `return_url`.

--- payment-element/client/vue-cva/vite.config.js
+++ payment-element/client/vue-cva/vite.config.js
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()], 
+  plugins: [vue()],
   server: {
+    host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:4242",
+        target: process.env.SERVER_URL || "http://localhost:4242",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
