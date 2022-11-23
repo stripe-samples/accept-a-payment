32a72f726666aac46a16cd8ea24b23db0b2298e2
Hibariya <hibariya@gmail.com>
2022-11-23 10:06:44 +0900

    Update Stripe API to 2022-08-01
    The SDK provides its type information for only the latest version

--- custom-payment-flow/server/node-typescript/src/server.ts
+++ custom-payment-flow/server/node-typescript/src/server.ts
@@ -9,7 +9,7 @@ import express from "express";
 
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2020-08-27",
+  apiVersion: "2022-08-01",
   appInfo: { // For sample support and debugging, not required for production:
     name: "stripe-samples/accept-a-payment",
     url: "https://github.com/stripe-samples",
