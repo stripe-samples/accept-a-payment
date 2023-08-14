8dcd806d7cc0bb0e41e097ca1fe063a6cfc3c615
Hibariya <hibariya@gmail.com>
2023-08-14 21:16:41 +0900

    Not used anymore

--- .devcontainer/generate.sh
+++ .devcontainer/generate.sh
@@ -20,22 +20,3 @@ rm -f sample-ci
 ln -sF ../.env .
 popd
 
-
-# sample="custom-payment-flow"
-# for lang in $(cat .cli.json | server_langs_for_integration "$sample")
-# do
-#   WORKING_DIR="${sample}/server/${lang}"
-#   CONFIG_DIR=$(echo "$WORKING_DIR" | tr / -)
-#   mkdir -p "$CONFIG_DIR"
-#   cat template.json | envsubst '$WORKING_DIR' > "$CONFIG_DIR/devcontainer.json"
-#   cp docker-compose.override.yml "$CONFIG_DIR/docker-compose.override.yml"
-# 
-#   # TODO: generate docker-compose.yml
-#   #   install_docker_compose_settings_for_integration
-#   #     export SAMPLE=${1}
-#   #     export SERVER_TYPE=${2}
-#   #     export STATIC_DIR=${3}
-#   #     export SERVER_IMAGE=${4}
-#   #     export CLIENT_TYPE=$(basename "$STATIC_DIR")
-# 
-# done
